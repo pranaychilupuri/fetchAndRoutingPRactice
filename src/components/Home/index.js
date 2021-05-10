a262083363011a4dc9fc15d0c36272c07a2197c8
pranaychilupuri <pranaychilupuri@gmail.com>
2021-05-10 12:26:02 +0000

    fetchAndRoutingPRactice

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import BlogList from '../BlogList'
+import UserInfo from '../UserInfo'
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <BlogList />
+    <UserInfo />
+  </div>
+)
+
+export default Home
